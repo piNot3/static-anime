09d3bab6870009bf4ba845cb8a40d662a3f86395
piNot3 <piNot3@tuta.io>
2016-09-17 13:45:48 -0400

    Some changes at async

--- index.js
+++ index.js
@@ -1,85 +1,83 @@
-var fs         = require("fs"),
-    async      = require("async"),
-    mkdirp     = require('mkdirp'),
-    mustache   = require("mustache"),
-    minifyhtml = require("html-minifier").minify;
+var fs         = require("fs")
+  , mkdirp     = require('mkdirp')
+  , parallel   = require("run-parallel")
+  , waterfall  = require("run-waterfall")
+  , mustache   = require("mustache")
+  , minifyhtml = require("html-minifier").minify
+  , anime = "some-anime"
+  , path = "site/json/" + anime + "/"
+  , read = function(path, callback) {
+        fs.readFile(path, callback);
+    }
+  , write = function(path, text, callback) {
+        fs.writeFile(path, text, callback);
+    };
 
-var anime = "some-anime";
-
-var path = "site/json/" + anime + "/";
-
-var list = fs.readdirSync(path);
-
-async.parallel({
-    json: (callback) => {
-        // Open some-anime.json
-        async.waterfall([
-            (callback) => {
-                fs.readFile("site/json/" + anime + ".json", callback);
-            },
-            (data) => {
-                callback(null, JSON.parse(data));
-            }
-        ], () => {
-            console.log("Error while reading json file")
-        });
+parallel({
+    files: function(callback) {
+        fs.readdir(path, callback);
     },
-    html: (callback) => {
-        // Open html
-        async.waterfall([
-            (callback) => {
-                fs.readFile("site/html/2.html", callback);
+    html: function(callback) {
+        read("site/html/2.html", callback);
+    },
+    json: function(callback) {
+        read("site/json/" + anime + ".json", callback);
+    }
+}, function(error, objects) {
+    if(error) return console.log("Error while reading html/json file!");
+
+    for(var n in objects.files) {
+        parallel({
+            folder: function(callback) { callback(null, "public/" + anime + "/" + objects.files[n].slice(0, -5)) },
+            obj1: function(callback) {
+                callback(null, JSON.parse(objects.json));
             },
-            (data) => {
-                callback(null, data.toString());
+            obj2: function(callback) {
+                read("site/json/some-anime/" + objects.files[n], function(error, text) {
+                if(error) return console.log("Error while reading json file!");
+                    callback(null, JSON.parse(text))
+                });
             }
-        ], () => {
-            console.log("Error while reading html file")
+        }, function(error, results) {
+            if(error) return console.log("Error while parsing json!");
+            waterfall([
+                function(callback) {
+                 // Return an object of obj1 + obj2
+                    callback(null, Object.assign({}, results.obj1, results.obj2));
+                },
+                function(object, callback) {
+                 // Return a rendered html from text + object
+                    callback(null, mustache.to_html(
+                        objects.html.toString(),
+                        object
+                    ));
+                },
+                function(html, callback) {
+                 // Return a minified html from html
+                    callback(null,
+                        minifyhtml(
+                            html,
+                            {
+                                removeAttributeQuotes: true,
+                                collapseWhitespace: true,
+                                collapseInlineTagWhitespace: true,
+                                removeComments: true
+                            }
+                        )
+                    );
+                },
+                function(html) {
+                    mkdirp(results.folder, function() {
+                        write(results.folder + "/index.html", html, callback);
+                    });
+                },
+                function() {
+                    console.log(results.folder)
+                }
+            ]);
+            //minify();
+            //render(objects.html.toString(), merge(objs.obj1, objs.obj2))
         });
     }
-}, (error, data) => {
- // Fired only if no error cause already error handler
-    async.map(list, (file) => {
-        async.waterfall([
-            (callback) => {
-             // Read file's text
-                fs.readFile(path + file, callback);
-            },
-            (text, callback) => {
-             // Parse text to json
-                callback(null, JSON.parse(text));
-            },
-            (json, callback) => {
-             // Merge two objects into one
-                callback(null, Object.assign({}, data.json, json));
-            },
-            (json, callback) => {
-             // Render from html file with json object
-                callback(null, mustache.to_html(data.html, json));
-            },
-            (html, callback) => {
-             // Minify html
-                callback(
-                    null,
-                    minifyhtml(html, {
-                        removeAttributeQuotes: true,
-                        collapseWhitespace: true,
-                        collapseInlineTagWhitespace: true,
-                        removeComments: true
-                    })
-                );
-            },
-            (html, callback) => {
-             // Make the folder
-                var folder = "public/" + anime + "/" + file.slice(0, -5);
-                mkdirp(folder, callback(null, folder, html));
-            },
-            (folder, text) => {
-             // Write new html file into folder "public"
-                fs.writeFile(folder + "/index.html", text);
-            }
-        ], () => {
-            console.log("Error while reading/fetching/writing data!");
-        });
-    });
 });
+// Wondering why need parallel and waterfall? It aloud me to write multiple file at the same time!
