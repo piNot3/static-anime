e313267b6743e999a2165df7e51a1fbda1845531
piNot3 <piNot3@tuta.io>
2016-09-16 20:14:40 -0400

    Trying to make it async

--- d.js
+++
@@ -1,2 +0,0 @@
-var staticAnime = require("./index.js");
-staticAnime.main();
--- index.js
+++ index.js
@@ -1,166 +1,228 @@
-var fs         = require("fs");
-var mustache   = require("mustache");
-var minifyhtml = require("html-minifier").minify;
-
-function readDir(path) {
-    
-}
-
-function readFile(path) {
-    
-}
-
-function openFile(path) {
-    return JSON.parse(readFile("site/json/" + path));
-}
-
-var render = function(path1, path2, content) {
-    // "site/html/" + n + ".html"
-};
-
-var $ = {
-    read: function(path) {
-        fs.readFile(path, function(err, content) {
-            if(!err & content) {
-                return content;
-            } else {
-                console.log("The file seem missing!");
-                process.exit(1);
-            }
-        });
+var fs         = require("fs"),
+    async      = require("async"),
+    mkdirp     = require('mkdirp'),
+    mustache   = require("mustache"),
+    minifyhtml = require("html-minifier").minify;
+/*
+async.waterfall([
+    (callback) => {
+        fs.readFile("site/json/some-anime.json", callback);
     },
-    write: function() {
-        fs.writeFile(path, content, function(err) {
-            if(err) {
-                console.log("Can't write on file!");
-                process.exit(1);
-            }
-        });
+    (data, callback) => {
+        callback(null, JSON.parse(data));
     },
-    lister: function(path) {
-        fs.readdir(path, function(err, files) {
-            if(!err & files) {
-                return files;
-            } else {
-                console.log("The folder seem missing!");
-                process.exit(1);
-            }
-        });
-    },
-    jsonify: function(content) {
-        return JSON.parse(content);
-    },
-    minify: function(content) {
-        return minifyhtml(content, {
-            removeAttributeQuotes: true,
-            collapseWhitespace: true,
-            collapseInlineTagWhitespace: true,
-            removeComments: true
-        });
-    },
-    render: function() {
-        return mustache.to_html(readFile(path), content);
-    },
-    build: function(path1, path2, content) {
-        write(
-            path1,
-            minify(
-                render(
-                    path2,
-                    content
-                )
-            )
-        );
+    (json) => {
+        console.log(json[0])
     }
-}
-    
-module.exports = {
-    main: function() {
+], () => {
+    console.log("Error while reading/fetching data!");
+});*/
 
-        var content = [];
+//fs.readdir(path, callback);
 
-        var list = $.lister("site/json/");
+var anime = "some-anime";
 
-        for(var i in list) {
+var path = "site/json/" + anime + "/";
 
-            if(list[i].slice(-5) == ".json") {
+var list = fs.readdirSync(path);
 
-                var json = $.jsonify( $.open("site/json/" + list[i]) );
-
-                content.push( [ json[0], json[1], json[2] ] );
-
+async.parallel({
+    json: (callback) => {
+        // Open some-anime.json
+        async.waterfall([
+            (callback) => {
+                fs.readFile("site/json/" + anime + ".json", callback);
+            },
+            (data) => {
+                callback(null, JSON.parse(data));
             }
-
-        }
-
-        build("public/index.html", "site/");
-
+        ], () => {
+            console.log("Error while reading json file")
+        });
     },
-    anime: function(anime) {
-        var infos = $.jsonify( $.open("site/json/" + anime + ".json") );
-
-        var content = {};
-        content.a = infos[0]; // a = some-anime
-        content.b = infos[1]; // b = Some Anime
-        content.c = infos[2]; // c = 2 Seasons * 25 Episodes
-        content.d = [];       // d = [ {"season": "Season 1", "list": [{"1-1": "Start"}]}, {"season": "Season 2", "list": [{"2-1": "End"}]} ]
-
-        // Read files from folder of the anime > site/json/some-anime/*
-        var files = $.lister("site/json/" + anime);
-
-        // Loop through the seasons of the list
-        for(var i in files) {
-
-            // Open json and parse object
-            var json = openJSON(anime + "/" + files[i]);
-
-            // $end the season name to list "content"
-            if(json.season!=undefined) {
-                content.d.push({ "season": json.season, "list": [] });
+    html: (callback) => {
+        // Open html
+        async.waterfall([
+            (callback) => {
+                fs.readFile("site/html/2.html", callback);
+            },
+            (data) => {
+                callback(null, data.toString());
             }
-
-            // Push object (with episode and title) to array "list"
-            content.d.slice(-1)[0].list.push({ "episode": json.episode, "title": json.title });
-        }
-
-		if(fs.existsSync("public/" + anime) == false) {
-            fs.mkdirSync("public/" + anime);
-        }
-
-        write(
-            "public/" + anime + "/index.json",
-            minify(
-                render(
-                    "site/html/1.html",
-                    content
-                )
-            )
-        );
-
-        console.log("Done.");
-    },
-    episode: function(anime, episode) {
-        var json = openJSON(anime + ".json");
-        var content = openJSON(anime + "/" + episode + ".json");
-        content.anime = json[1];
-        if(fs.existsSync("public/" + anime + "/" + episode) == false) {
-            fs.mkdirSync("public/" + anime + "/" + episode);
-        }
-        write(
-            "public/" + anime + "/" + episode + "/index.json",
-            minify(
-                render(
-                    "site/html/2.html",
-                    content
-                )
-            )
-        );
-        console.log("Done.");
+        ], () => {
+            console.log("Error while reading html file")
+        });
     }
-}
+}, (error, data) => {
+ // Fired only if no error cause already error handler
+    async.map(list, (file) => {
+        async.waterfall([
+            (callback) => {
+             // Read file's text
+                fs.readFile(path + file, callback);
+            },
+            (text, callback) => {
+             // Parse text to json
+                callback(null, JSON.parse(text));
+            },
+            (json, callback) => {
+             // Merge two objects into one
+                callback(null, Object.assign({}, data.json, json));
+            },
+            (json, callback) => {
+             // Render from html file with json object
+                callback(null, mustache.to_html(data.html, json));
+            },
+            (html, callback) => {
+             // Minify html
+                callback(
+                    null,
+                    minifyhtml(html, {
+                        removeAttributeQuotes: true,
+                        collapseWhitespace: true,
+                        collapseInlineTagWhitespace: true,
+                        removeComments: true
+                    })
+                );
+            },
+            (html, callback) => {
+             // Make the folder
+                var folder = "public/" + anime + "/" + file.slice(0, -5);
+                mkdirp(folder, callback(null, folder, html));
+            },
+            (folder, text) => {
+             // Write new html file into folder "public"
+                fs.writeFile(folder + "/index.html", text);
+            }
+        ], () => {
+            console.log("Error while reading/fetching/writing data!");
+        });
+    });
+});
+/*
+async.waterfall([
+    () => {
+        
+    }
+    (callback) => {
+        
+    },
+    (data, callback) => {
+        
+    },
+    (html) => {
+        
+    }
+]);
+*/
 
-// 1 = path/to/$.js
-// 2 = 0 or 1 or 2
-// 3 = some-anime
-// 4 = 1-1
-// $[process.argv[2]](process.argv[3], process.argv[4]);
+// async.parallel();
+/*
+async.parallel([
+    function() {
+        console.log("0")
+        fs.readFile("site/json/some-anime/0.json", function(e, data) {
+            console.log(JSON.parse(data))
+        });
+    },
+    function() {
+        console.log("1")
+        fs.readFile("site/json/some-anime/1.json", function(e, data) {
+            console.log(JSON.parse(data))
+        });
+    }
+],
+// optional callback
+function(err, results) {
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+    console.log(results)
+});*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var $ = {}
+    
+module.exports = {}
+
+var data = "";
+var file = "site/json/some-anime.json";
+var html = fs.createReadStream("site/html/0.html");
+
+var files = fs.readdirSync("site/json/some-anime/");
+
+for(var file in files) {
+    if(file.slice(-5) != ".json")
+        continue
+    jsonfile.readFile("site/jon/some-anime/" + file, function(err, obj) {
+      console.log(err)
+    })
+}*/
+
+/*
+fs.createReadStream("site/json/some-anime/1-1.json" )
+.on("data", (chunk) => {
+    data += chunk; // 1
+}).on("end", () => {
+    console.log(data); // 3
+});
+
+fs.createReadStream("site/json/some-anime/1-2.json" )
+.on("data", (chunk) => {
+    data += chunk;  // 2
+}).on("end", () => {
+    console.log(data); // 4
+});
+
+
+
+html.on("data", (chunk) => {
+    data += chunk;
+});
+
+var json = fs.createReadStream("site/json/0.html");
+
+read.on("data", (chunk) => {
+    data += chunk;
+});
+
+read.on("end", () => {
+    //console.log(data);
+});
+
+read.on("error", () => {
+    console.log("Error while reading the file");
+});
+
+console.log("hello");*/
+/*
+var fileToWrite = fs.createWriteStream("test.txt");
+
+fileToWrite.on('finish', function() {
+    console.log("Write completed.");
+});
+
+fileToWrite.write("hello my name is issam");
+fileToWrite.end("coco");*/
+
+/*
+var writer = fs.createWriteStream("test.txt");
+read.pipe(writer);
+
+writer.on("finish", () => {
+    console.log("J'ai lu le fichier");
+});*/
